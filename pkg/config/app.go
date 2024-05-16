103bef868f39486fa5ab9e3ee7f01cc36ea47420
martoast <alexmartos96@gmail.com>
2024-05-16 11:54:19 -0700

    username from env for mysql connection

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -13,17 +13,18 @@ var (
 )
 
 func Connect() {
+	dbusername, dbusername_ok := os.LookupEnv("MYSQL_USERNAME")
 	dbname, dbname_ok := os.LookupEnv("MYSQL_DATABASE")
 	rootpass, rootpass_ok := os.LookupEnv("MYSQL_ROOT_PASSWORD")
 	dbhost, dbhost_ok := os.LookupEnv("MYSQL_HOST")
 	dbport, dbport_ok := os.LookupEnv("MYSQL_PORT")
-	if dbname_ok && rootpass_ok && dbhost_ok && dbport_ok {
+	if dbusername_ok && dbname_ok && rootpass_ok && dbhost_ok && dbport_ok {
 		fmt.Println(rootpass, dbname, dbhost)
 	} else {
 		fmt.Println("Environment variables are not set. Please set environment variables.")
 	}
 
-	dsn := fmt.Sprintf("root:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", rootpass, dbhost, dbport, dbname)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbusername, rootpass, dbhost, dbport, dbname)
 
 	fmt.Println(dsn)
 
